a70f352cdd57d8f18757db8696d7af7d205f64be
ZinedineZethro <zinedine.zethro@gmail.com>
2023-03-11 23:19:47 +0700

    more updates

--- php/login.php
+++ php/login.php
@@ -1,4 +1,17 @@
 <?php
     include('connect.php');
-    include('')
+    include('customer.php');
+
+    $cust = new customer();
+    
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $sub_type = $_POST['submit_type'];
+        $username = $_POST['username'];
+        $password_get = $_POST['password'];
+        $password = password_hash($password_get, PASSWORD_DEFAULT);
+
+        if($sub_type === 'login'){
+            $cust->login($username, $password);
+        }
+    }
 ?>
