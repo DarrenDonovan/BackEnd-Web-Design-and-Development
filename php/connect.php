bbd60c5620652d081eaa3b7ba1852fcbccb667c1
ZinedineZethro <zinedine.zethro@gmail.com>
2023-06-15 16:20:20 +0700

    clean up

--- php/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $serverName = "DELL\\SQLEXPRESS";
-    $connectionInfo = array("Database" => "ezgo");
-    $conn = sqlsrv_connect($serverName, $connectionInfo);
-    if (!$conn) {
-        die("Connection failed: " . sqlsrv_errors());
-    }
-?>
